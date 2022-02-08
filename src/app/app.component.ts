dc594b24854c60a70373c83118d5bc47f8988df4
shawn <shawntan2@gmail.com>
2022-02-08 09:34:54 +0800

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { todoObject } from './todoObject';
 
 
@@ -18,8 +19,13 @@ export class AppComponent {
   taskn:string="";
   dued:Date=new Date;
   prioritylvl="";
-  idnum:number =0;
+  idnum:number=0;
   todomap=new Map();
+
+  StoredMap=new Map();
+
+  teststring:any;
+
 
   priority = ["low", "medium", "high"];
 
@@ -29,20 +35,23 @@ export class AppComponent {
 
   constructor(private fb:FormBuilder){
 
-    /*  this.groupedform=this.fb.group({
-      taskname:new FormControl(),
-      dueDate:new FormControl([Validators.required]),
-      prioritylevel:new FormControl(),
-
-    }
-    ) */
-    this.groupedform=this.fb.group({
+      this.groupedform=this.fb.group({
       taskname:this.taskname,
       dueDate:this.dueDate,
       prioritylevel:this.prioritylevel
     })
   }
+  ngOnInit(): void{
 
+    for (let x=0;x<localStorage.length;x++)
+    {
+      let xx=localStorage.key(x) as string;
+      let y=localStorage.getItem(xx) as string;
+      this.todomap.set(xx,JSON.parse(y))
+      //this.idnum=x+1;
+    }
+
+  }
   onsubmit():void{
     this.dued=this.groupedform.value.dueDate;
     if (this.groupedform.value.dueDate<this.today){
@@ -50,26 +59,28 @@ export class AppComponent {
     } //end if
     else{
     this.taskn=this.groupedform.value.taskname;
-
     this.prioritylvl=this.groupedform.value.prioritylevel;
-    this.idnum=this.idnum+1;
-    //this.idnum=this.groupedform.value.idid;
+    //this.idnum=this.idnum+1;
+
 
     let todoagenda=new todoObject(
       this.groupedform.value.taskname,
       this.groupedform.value.dueDate,
       this.groupedform.value.prioritylevel,
       //this.groupedform.value.idid
-      this.idnum
+      this.idnum=this.idnum+1
     );
 
-    this.todomap.set(this.idnum,todoagenda);
-    console.log("map is "+this.todomap.get(1).taskname)
+
+    localStorage.setItem(JSON.stringify(todoagenda.taskname),JSON.stringify(todoagenda));
+
+   // this.todomap.set(JSON.stringify(todoagenda.taskname),JSON.stringify(todoagenda));
+
     }//end else
   }
 
-  onremove(idid:number):void{
-    this.todomap.delete(idid);
-
+  onremove(idid:string):void{
+   // this.todomap.delete(idid);
+    localStorage.removeItem(idid)
   }
 }
